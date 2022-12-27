d7620483d628c91aa23d64775875b88c03a4deb4
yday05506 <yangdayeon2@gmail.com>
2022-12-27 11:26:31 +0900

    matching

--- day2/LoopExample.java
+++ day2/LoopExample.java
@@ -15,5 +15,19 @@ public class LoopExample {
         System.out.println("===============================");
 
         intStream1.filter(a->a%2==1).forEach(n->System.out.println(n));
+
+        System.out.println("===============================");
+
+        // 매칭
+        int[] intArray1 = {2,4,6};
+
+        boolean result = Arrays.stream(intArray1).allMatch(a->a%2==0);  // 다 맞을 때
+        System.out.println("결과 : " + result);
+
+        result = Arrays.stream(intArray1).anyMatch(a->a%3==0);  // 하나라도 맞으면
+        System.out.println("결과 : " + result);
+
+        result = Arrays.stream(intArray1).noneMatch(a->a%4==0); // 맞지 않을 때
+        System.out.println("결과 : " + result);
     }
 }
