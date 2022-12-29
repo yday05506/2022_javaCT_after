3bbfdcd251ac1239da2807b058f10f4c533d617d
yday05506 <yangdayeon2@gmail.com>
2022-12-29 10:40:19 +0900

    예산(이진탐색)

---
+++ day4/Budgets.java
@@ -0,0 +1,39 @@
+package day4;
+
+import java.util.Arrays;
+
+class Budgets {
+    public int solution(int[] budgets, int M) {
+        int answer = 0;
+
+        // 최소값
+        int min = 0;
+        // 최대값 
+        int max = 0;
+
+        for(int b : budgets) {
+            if(b > max) max = b;
+        }
+
+        // 최대값, 최소값 비교(이진탐색)
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            int sum = 0;
+
+            for(int b : budgets) {
+                if(b > mid) // 신청한 예산이 상한액보다 큰 경우
+                    sum += mid;
+                else sum += b;  // 신청한 값으로
+            }
+
+            // 총 합계 금액과 총 예산 비교
+            if(sum <= M) {
+                min = mid + 1;  // 최소값 새 조정
+                answer = mid;
+            }
+            else max = mid - 1;
+        }
+
+        return answer;
+    }
+}
