51f525bd8186f4c9dd54c222cd8756bed9ea4ec8
yday05506 <yangdayeon2@gmail.com>
2022-12-29 15:38:22 +0900

    완주하지 못한 선수 (HashMap 사용)

---
+++ day4/DontFinish2.java
@@ -0,0 +1,23 @@
+package day4;
+
+import java.util.*;
+
+class DontFinish2 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> map = new HashMap();
+
+        for(String player : participant) map.put(player, map.getOrDefault(player, 0) + 1);
+        for(String player : completion) map.put(player, map.get(player) - 1);
+        for(String key : map.keySet()) {
+            if(map.get(key)!=0) {
+                answer = key;
+                break;
+            }
+        }
+
+        return answer;
+
+    }
+}
